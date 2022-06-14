a28cac0f18ee55b163ae31ef67a5bdccd91197cd
bioss7 <fireleny@gmail.com>
2022-06-14 10:33:44 +0700

    custom hook example 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import React from "react";
-import Main from "./Main";
-import Alert from "./alert/Alert";
-import { AlertProvider } from "./alert/АlertContext";
+import React, { useEffect, useState } from "react";
+
+// Создание своего hook
+function useLogger(value) {
+    useEffect(() => {
+        console.log('Value changed: ', value);
+    }, [value]);
+}
+
+function useInput(initialValue) {
+    const [value, setValue] = useState(initialValue);
+
+    const onChange = event => {
+        setValue(event.target.value)
+    }
+
+    return {
+        value, onChange
+    }
+}
 
 function App() {
-    
+    const [name, setName] = useState('');
+
+    const changeHandler = event => {
+        setName(event.target.value)
+    }
+
+    useLogger(name);
+
     return (
-        <AlertProvider>
-            <div className={'container pt-3'}>
-                <Alert />
-                <Main toggle={() => {}}/>
-            </div>
-        </AlertProvider>
+        <div className={'container pt-3'}>
+            <input type="text" value={name} onChange={changeHandler} />
+            <hr />
+            <h1>{name}</h1>
+        </div>
     )
 
 }
